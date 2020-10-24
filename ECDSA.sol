b13acb23b146984c2e8ecf867f82123ddbb37385
vatsal <41521501+vatsal095@users.noreply.github.com>
2020-10-24 23:59:30 +0530

    Update ECDSA.sol

--- ECDSA.sol
+++ ECDSA.sol
@@ -1,11 +1,6 @@
 pragma solidity ^0.6.0;
 
-/**
- * @dev Elliptic Curve Digital Signature Algorithm (ECDSA) operations.
- *
- * These functions can be used to verify that a message was signed by the holder
- * of the private keys of a given address.
- */
+
 library ECDSA {
     /**
      * @dev Returns the address that signed a hashed message (`hash`) with
